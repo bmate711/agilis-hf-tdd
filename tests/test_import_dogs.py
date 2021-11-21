cc1979927afc6543410add0bb6657c40d61b3002
Mate Bodor <mate.bodor@zooshgroup.com>
2021-11-21 11:43:44 +0100

    Import dogs data test: dog name must not contain cat

--- tests/test_import_dogs.py
+++ tests/test_import_dogs.py
@@ -1,5 +1,6 @@
 from agilisHF.import_dogs import import_data
 import mongomock
+import pytest
 import unittest
 from agilisHF.controllers import ValidationError, get_details_by_search
 from agilisHF.model import Dog
@@ -14,7 +15,12 @@ class DetailsTest(unittest.TestCase):
         self.db.dogs.drop()
         return super().tearDown()
 
-    def test_import_data_pass_return_value_true(self):
+    def test_import_data_return_value_true(self):
         raw = [{'name': 'test', 'color': 'brown', 'description': 'description!! brown textetxtetxtettetttedtetdtedtedtetdetdettetetedtetd', 'breed': 'finally!!', 'vaccination': ['v1!!', 'vocid', 'cutness'], 'age': 10, 'sex': False}]
         result = import_data(raw, self.db)
         assert True == result
+
+    def test_import_data_dog_exception_name_must_not_contain_cat(self):
+        raw = [{'name': 'tecatst', 'color': 'brown', 'description': 'description!! brown textetxtetxtettetttedtetdtedtedtetdetdettetetedtetd', 'breed': 'finally!!', 'vaccination': ['v1!!', 'vocid', 'cutness'], 'age': 10, 'sex': False}]
+        with pytest.raises(ValidationError):
+            import_data(raw, self.db)
