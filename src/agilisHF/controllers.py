8977fe97e98e9e098d47c1c00bd9dfaa5b76dbf0
Chif Gergő <chifgeri97@gmail.com>
2021-11-23 20:30:49 +0100

    Add search condition key validation

--- src/agilisHF/controllers.py
+++ src/agilisHF/controllers.py
@@ -21,16 +21,25 @@ class ValidationError(Exception):
 #   "vaccinated": boolean
 #   "color": string
 #   "sex": boolean
-#   "search_str": string,
 #   "breed": string
 # }
 def get_details_by_search(search_conditions: dict, pymongo_db: Database) -> List[Dog]:
+    validate_search_condition_keys(search_conditions)
     result = pymongo_db.dogs.find(search_conditions)
     dogs: List[Dog] = []
     for dog in result:
         dogData = Dog(**dog)
         dogs.append(dogData.to_json())
     return dogs
+
+
+valid_keys = ["name", "age", "vaccinated", "color", "sex", "breed"]
+
+
+def validate_search_condition_keys(search_conditions: dict):
+    for key in search_conditions.keys():
+        if key not in valid_keys:
+            raise ValidationError(f"Wrong key in search condition: {key}")
 
 
 def get_details_by_id(id: str, pymongo_db: Database):
