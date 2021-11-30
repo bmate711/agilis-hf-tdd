2d19ae776f510fb845f6eac459f0daacaa6292ef
Chif Gergő <chifgeri97@gmail.com>
2021-11-30 12:16:08 +0100

    Remove unnecessary comments

--- src/agilisHF/import_dogs.py
+++ src/agilisHF/import_dogs.py
@@ -9,13 +9,6 @@ class ValidationError(Exception):
     pass
 
 
-# Adatbázisban tárolni kell az új kutyákat
-# A kutya neve nem tartalmazhatja a 'cat' szót
-# Ha a kutya fiú 'M' kell kezdődni a neve
-# Ha a kutya 5 évnél idősebb, rendelkezni kell 'cuteness' oltással
-# Ha a kutya keverék, akkor szerepelnie kell a nevében 'Snow' szónak
-# A leírás 50-100 karakte hosszú lehet
-# A kutya színének szerepelnie kell a leírásban
 def import_data(raw_dog_list, db_context):
     validate_dog_list(raw_dog_list)
     dogs = []
