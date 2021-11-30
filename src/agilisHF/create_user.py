dc7da629c66463902e8bbd8f4d026705a5655b55
Mate Bodor <mate.bodor@zooshgroup.com>
2021-11-30 17:50:20 +0100

    Refactor create user

---
+++ src/agilisHF/create_user.py
@@ -0,0 +1,61 @@
+from agilisHF.objectid import PydanticObjectId
+from .model import User
+import re
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+regex2 = r'^[a-zA-Z]+" "[a-zA-Z]+$'
+# regex3 = r'\b[0-9]{9}+[A-Z]{2}\b'
+regex3 = r'[0-9]{9}[A-Z]{2}'
+
+class ValidationKeyError(KeyError):
+    pass
+
+class ValidationError(Exception):
+    pass
+
+def create_user(raw, db):
+    validate_fullname(raw)
+    validate_idcardnumber(raw)
+    validate_emailaddress(raw, db)
+    validate_password(raw)
+    user = User(**raw)
+    insert_result = db.insert_one(user.to_bson())
+    user.id = PydanticObjectId(str(insert_result.inserted_id))
+    return True
+
+def validate_fullname(raw):
+    fullname = raw["fullname"]
+    match = re.fullmatch(regex2, fullname)
+    print(match)
+    if len(fullname) <= 2 or len(fullname) > 128:
+        raise ValidationError("Invalid name, a name length should be between 2 and 128") 
+    if (re.fullmatch(regex2, fullname)):
+        raise ValidationError("Invalid name, must contain a first name and a last name and can't contain numbers")
+
+def validate_idcardnumber(raw):
+    idcardnumber = raw["idcardnumber"]
+    if len(idcardnumber) != 11:
+        raise ValidationError("ID must be 11 character")
+    if type(idcardnumber) is not str:
+        raise ValidationError("Idcardnumber value must be a valid string")
+    if (not re.fullmatch(regex3, idcardnumber)):
+        raise ValidationError("ID must in correct format 9 number then 2 character A-Z")
+
+
+def validate_emailaddress(raw, db):
+    email = raw['emailaddress']
+    if(not re.fullmatch(regex, email)):
+        raise ValidationError("Invalid email form")
+    users=db.users
+    existing_user=users.find_one({"emailadress":email})
+    if not existing_user is None :
+        raise ValidationError("Email address is already exist")
+
+def validate_password(raw):
+    password =  raw['password']
+    if not re.search('[A-Z]', password) or not re.search('[0-9]', password):
+        raise ValidationError("It must contain capital letter, and number")
+
+    
+
+
+
--- tests/test_import_user.py
+++ tests/test_import_user.py
@@ -1,7 +1,7 @@
-from agilisHF.import_user import import_data
+from agilisHF.create_user import create_user
 import mongomock 
 import unittest
-from agilisHF.import_user import ValidationError
+from agilisHF.create_user import ValidationError
 from agilisHF.model import User
 
 class DetailsTest(unittest.TestCase):
@@ -14,54 +14,55 @@ class DetailsTest(unittest.TestCase):
         self.db.drop()
         return super().tearDown()
 
-    def test_import_data_return_value_true(self):
-        raw=[{'fullname':'Test Name','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}]
+    def test_create_user_return_value_true(self):
+        raw={'fullname':'Test Name','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}
 
-        result = import_data(raw, self.db)
+        result = create_user(raw, self.db)
         assert True == result
 
 
-    def test_import_data_user_name_must_contain_space(self):
-        raw=[{'fullname':'TestName','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}]
+    def test_create_user_user_name_must_contain_space(self):
+        raw={'fullname':'TestName','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
-        self.assertTrue("Name doesn't contain space character" in str(context.exception))
+            result = create_user(raw, self.db)
+        self.assertTrue("Invalid name, must contain a first name and a last name and can't contain numbers" in str(context.exception))
 
 
-    def test_import_data_user_name_must_be_longer_than_two_character(self):
-        raw=[{'fullname':'T ','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}]
+    def test_create_user_user_name_must_be_longer_than_two_character(self):
+        raw={'fullname':'T ','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
-        self.assertTrue("Name is shorter than two character" in str(context.exception))
+            result = create_user(raw, self.db)
+        self.assertTrue("Invalid name, a name length should be between 2 and 128" in str(context.exception))
 
-    def test_import_data_user_name_must_be_shorter_than_128_character(self):
-        raw=[{'fullname':'ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}]
+    def test_create_user_user_name_must_be_shorter_than_128_character(self):
+        raw={'fullname':'ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF ABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEFABCDEF','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
-        self.assertTrue("Name is longer than 128 character" in str(context.exception))
+            result = create_user(raw, self.db)
+        self.assertTrue("Invalid name, a name length should be between 2 and 128" in str(context.exception))
 
-    def test_import_data_user_emailaddress_must_be_in_correct_format(self):
-        raw=[{'fullname':'Test Name','idcardnumber':'123456789AB','emailaddress':'asdikaplscom','password':'#Artirw213'}]
+    def test_create_user_user_emailaddress_must_be_in_correct_format(self):
+        raw={'fullname':'Test Name','idcardnumber':'123456789AB','emailaddress':'asdikaplscom','password':'#Artirw213'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
-        self.assertTrue("Email must be in a correct email format" in str(context.exception))
+            result = create_user(raw, self.db)
+        self.assertTrue("Invalid email form" in str(context.exception))
 
-    def test_import_data_user_idcardnumber_must_be_11_character(self):
-        raw=[{'fullname':'Test Name','idcardnumber':'123452131231231236789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}]
+    def test_create_user_user_idcardnumber_must_be_11_character(self):
+        raw={'fullname':'Test Name','idcardnumber':'123452131231231236789AB','emailaddress':'asdika@pls.com','password':'#Artirw213'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
+            result = create_user(raw, self.db)
         self.assertTrue("ID must be 11 character" in str(context.exception))
 
-    def test_import_data_user_idcardnumber_must_be_in_correct_format(self):
-        raw=[{'fullname':'Test Name','idcardnumber':'123456AB789','emailaddress':'asdika@pls.com','password':'#Artirw213'}]
+    def test_create_user_user_idcardnumber_must_be_in_correct_format(self):
+        raw={'fullname':'Test Name','idcardnumber':'123456AB789','emailaddress':'asdika@pls.com','password':'#Artirw213'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
+            result = create_user(raw, self.db)
         self.assertTrue("ID must in correct format 9 number then 2 character A-Z" in str(context.exception))
     
 
-    def test_import_data_user_password_must_contain_capital_letter_and_number(self):
-        raw=[{'fullname':'Test Name','idcardnumber':'123456AB789','emailaddress':'asdika@pls.com','password':'asdikaplusone'}]
+    def test_create_user_user_password_must_contain_capital_letter_and_number(self):
+        raw={'fullname':'Test Name','idcardnumber':'123456789AB','emailaddress':'asdika@pls.com','password':'asdikaplusone'}
         with self.assertRaises(ValidationError) as context:
-            result = import_data(raw, self.db)
-        self.assertTrue("Password must contain capital letter and number" in str(context.exception))
+            result = create_user(raw, self.db)
+        print(context.exception)
+        self.assertTrue("It must contain capital letter, and number" in str(context.exception))
     
